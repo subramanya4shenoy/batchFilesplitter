8cee1268ed19656b8627719069cdb5e3d9f02f38
Subramanya Shenoy <subramanya4shenoy@gmail.com>
2017-07-20 11:32:43 +0530

    Update god.bat

--- god.bat
+++ god.bat
@@ -4,16 +4,34 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 
 SET foldercount=5
 
+SET "file=file/file_list.txt"
+
+
+SET /A i=0
+
 FOR /l %%a in (1, 1, %foldercount%) do ( MD "Folder"%%a )
+
+FOR /F "usebackq delims=" %%a in ("%file%") DO (
+
+SET /A i+=1
+
+CALL SET array[%%i%%]=%%a
+
+CALL SET m=%%i%%
+)
 
 SET n=1
 
-FOR %%f IN (*.txt) DO (
+REM FOR /L %%i in (1,1,%m%) do call echo %%array[%%i]%%
+
+REM FOR %%f IN (*.txt) DO (
+
+FOR /L %%i in (1,1,%m%) DO (
   IF !n! == !foldercount! (
-    MOVE /Y "%%f" Folder!n!
+    MOVE /Y ./!array[%%i]! Folder!n!
     SET /A n=1
   ) ELSE (
-	MOVE /Y "%%f" Folder!n!
+	MOVE /Y !array[%%i]! Folder!n!
 	SET /A n+=1
   )
 )
